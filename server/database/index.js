abf207ab0c6702ca1b4d9ee20878a11ce3ee58d1
tamilselvankrishnamurthigit2197 <tamilselvanmoorthi97@gmail.com>
2025-02-08 13:51:22 +0530

    env files added for both client and server

--- server/database/index.js
+++ server/database/index.js
@@ -4,4 +4,4 @@ const mongoose = require("mongoose");
 
 /* mongodb+srv://tamilselvanmoorthi97:<db_password>@taskmanager.md0ug.mongodb.net/ */
 
- mongoose.connect('mongodb+srv://tamilselvanmoorthi97:tamilmongodbc2@cluster0.dxhby.mongodb.net/').then(()=> console.log('Mongoose connection is successfull')).catch((error)=> console.log(`Error is occured: ${error}`)); 
+ mongoose.connect(process.env.MONGODB_URL).then(()=> console.log('Mongoose connection is successfull')).catch((error)=> console.log(`Error is occured: ${error}`)); 
--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
@@ -10,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: process.env.CLIENT_BASE_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
@@ -26,4 +28,4 @@ app.use("/api", (req, res) => {
   res.status(200).json({ message: "Hello Express" });
 });
 /* PORT FROM ENV FILE */
-app.listen(5000, () => console.log(`App is now running and listening at port 5000...`));
+app.listen(PORT || 5000, () => console.log(`App is now running and listening at port 5000...`));
